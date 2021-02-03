b4b43cc6abc042ec02622d17d53c145345ff4d52
AndZeka <andizeka@hotmail.com>
2021-02-03 22:05:24 +0100

    Implementing logout

--- src/pages/Chat.vue
+++ src/pages/Chat.vue
@@ -5,7 +5,7 @@
                 <div class="col-md-4 sidebar">
                     <h2 class="text-light">#SLACK#</h2>
                     <hr style="border: 1px solid #333;">
-                    <button class="btn btn-outline-light">
+                    <button @click="logout" class="btn btn-outline-light">
                         Logout
                     </button>
                 </div>
@@ -18,8 +18,17 @@
 </template>
 
 <script>
+import auth from 'firebase/auth'
 export default {
-    name: 'chat'
+    name: 'chat',
+
+    methods:{
+        logout(){
+            firebase.auth().signOut();
+            this.$store.dispatch('setUser', null);
+            this.$router.push('/login');
+        }
+    }
 }
 </script>
 
