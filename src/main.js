b2648dccdb64c93f0910dce2075604953b2f5cde
AndZeka <andizeka@hotmail.com>
2021-02-03 15:12:50 +0100

    Firebase setup

--- src/main.js
+++ src/main.js
@@ -3,8 +3,23 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import firebase from 'firebase/app'
 
 Vue.config.productionTip = false
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyCx0pU7zJg_9cbwlzHnacR8WWRaRqdoEG4",
+  authDomain: "slack-clone-83e3b.firebaseapp.com",
+  projectId: "slack-clone-83e3b",
+  storageBucket: "slack-clone-83e3b.appspot.com",
+  messagingSenderId: "851119436827",
+  appId: "1:851119436827:web:6eec82f9844ed35837b4be"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+window.firebase = firebase;
 
 /* eslint-disable no-new */
 new Vue({
