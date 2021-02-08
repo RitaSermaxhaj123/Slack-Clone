5745a5a1179b91c17d70944c8b2f136358da57b7
RitaSermaxhaj123 <78446239+RitaSermaxhaj123@users.noreply.github.com>
2021-02-09 00:54:40 +0100

    Getting the messages

--- src/messages/Messages.vue
+++ src/messages/Messages.vue
@@ -11,7 +11,7 @@
 import SingleMessage from './SingleMessage'
 import MessageForm from './MessageForm'
 import database from 'firebase/database'
-
+import {mapGetters} from 'vuex'
 export default {
     name: 'messages',
 
@@ -19,8 +19,35 @@ export default {
 
     data(){
         return{
-            messagesRef: firebase.database().ref('messages')
+            messagesRef: firebase.database().ref('messages'),
+            messages:[],
+            channel:''
         }
+    },
+    computed: {
+        ...mapGetters(['currentChannel'])
+    },
+    watch:{
+        currentChannel : function(){
+            this.messages = []
+            this.addListeners()
+            this.channel = this.currentChannel
+        }
+    },
+    methods: {
+        addListeners(){
+            this.messagesRef.child(this.currentChannel.id).on('child_added', (snapshot) => {
+                this.messages.push(snapshot.val())
+            })
+        },
+        detachListeners(){
+            if(this.channel !== null){
+                this.messagesRef.child(this.channel.id).off()
+            }
+        }
+    },
+    beforeDestroy(){
+        this.detachListeners()
     }
 
 }
