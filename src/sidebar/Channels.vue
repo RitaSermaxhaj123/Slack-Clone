0e7de30f4447c46893ad41719ce86f633ae65e96
rinakasabaqi <rinakasabaqi7@gmail.com>
2021-03-09 22:05:29 +0100

    counting notifications

--- src/sidebar/Channels.vue
+++ src/sidebar/Channels.vue
@@ -61,6 +61,8 @@ import {mapGetters} from 'vuex'
                 new_channel:'',
                 errors:[],
                 channelRef: firebase.database().ref('channels'),
+                messageRef: firebase.database().ref('messages'),
+                notifCount: [],
                 channels: [],
                 channel: null
             }
@@ -107,9 +109,42 @@ import {mapGetters} from 'vuex'
                   //dispatch current channel to store
                   this.$store.dispatch("setCurrentChannel", this.channel) //pick the first one
               }
+              //add count listener to manage the notifications
+              this.addCountListener(snapshot.key)
                 })
            },
            
+           addCountListener(channelId){
+               this.messageRef.child(channelId).on('value', snapshot =>{
+                   this.handleNotifications(channelId, this.currentChannel.id, this.notifCount, snapshot) 
+               })
+           },
+
+           handleNotifications(channelId, currentChannelId, notifCount, snapshot){
+               let lastTotal = 0
+               //find if channelId is already added to notifCount[]
+               let index = notifCount.findIndex(el => el.id === channelId)
+                //if found
+                if(index != -1){
+                    if(channelId !== currentChannelId){
+                        lastTotal = notifCount[index].total
+
+                        if(snapshot.numChildren() - lastTotal > 0){
+                            notifCount[index].notif = snapshot.numChildren() - lastTotal
+                        }
+                    }
+                    notifCount[index].lastKnownTotal = snapshot.numChildren()
+                } else{
+                    //push to notifCount[]
+                    notifCount.push({
+                        id: channelId,
+                        total: snapshot.numChildren(),
+                        lastKnownTotal: snapshot.numChildren(),
+                        notif: 0
+                    })
+                }
+
+           },
            // set active channel
            setActiveChannel(channel){
                return channel.id === this.currentChannel.id
